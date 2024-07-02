bacfde510f1203daf09d329512671bfe8be544de
ndpisme137 <nguyenphuc6a15.8@gmail.com>
2024-07-02 18:55:29 +0700

    add file

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Counter App</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    ...mapMutations(['increment', 'decrement'])
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Counter</h1>
+      <p>Current count: {{ currentCount }}</p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+  </template>
+  
+  <script>
+  import { mapState, mapActions } from 'vuex'
+  
+  export default {
+    computed: {
+      ...mapState(['count']),
+      currentCount() {
+        return this.$store.getters.currentCount
+      }
+    },
+    methods: {
+      ...mapActions(['increment', 'decrement'])
+    }
+  }
+  </script>
